0f40565aed43483802555b307be10bbd8b1ced6f
summer-Zxc <1019851086@qq.com>
2017-09-18 00:40:27 +0800

    CountDownLawn

---
+++ src/com/fc/one/CountDownLawnTest.java
@@ -0,0 +1,49 @@
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import ga.hello2;
+
+public class CountDownLawnTest {
+	public long timeTasks(int nThreads, final Runnable task) throws InterruptedException {
+		final CountDownLatch startGate = new CountDownLatch(1);
+		final CountDownLatch endGate = new CountDownLatch(nThreads);
+		for (int i = 0; i < nThreads; i++) {
+			Thread t = new Thread() {
+				public void run() {
+					try {
+						startGate.await();
+						try {
+							task.run();
+						} finally {
+							endGate.countDown();
+						}
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			};
+			t.start();
+		}
+		long start = System.nanoTime();
+		startGate.countDown();
+		endGate.await();
+		long end = System.nanoTime();
+		return start - end;
+	}
+
+	public static void main(String[] args) {
+		final AtomicInteger ai = new AtomicInteger(0);
+		CountDownLawnTest h = new CountDownLawnTest();
+		try {
+			Long s = h.timeTasks(10, new Runnable() {
+				public void run() {
+					System.out.println(ai.getAndIncrement());
+					System.out.println(Thread.currentThread().getName());
+				}
+			});
+			System.out.println(s);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
