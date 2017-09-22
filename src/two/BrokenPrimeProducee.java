ae555b5a0f1a86d7311d062d3cc458daa2f61415
summer-Zxc <1019851086@qq.com>
2017-09-22 23:02:59 +0800

    带有中断的消费者模型

---
+++ src/two/BrokenPrimeProducee.java
@@ -0,0 +1,46 @@
+package two;
+
+import java.math.BigInteger;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+/**
+ * 带有中断的消费者模型
+ * @author liufanwen
+ *
+ * @date:2017年9月22日
+ */
+public class BrokenPrimeProducee extends Thread {
+	private final BlockingQueue<BigInteger> queue;
+	private volatile boolean cancelled = false;
+
+	public BrokenPrimeProducee(BlockingQueue<BigInteger> queue) {
+		this.queue = queue;
+	}
+
+	public void run() {
+		try {
+			BigInteger bi = BigInteger.ONE;
+			while (!cancelled) {
+				queue.put(bi.nextProbablePrime());
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void cancel() {
+		cancelled = true;
+	}
+
+	void consumePrimes() throws InterruptedException {
+		BlockingQueue<BigInteger> primes = new LinkedBlockingQueue<>();
+		BrokenPrimeProducee bp = new BrokenPrimeProducee(primes);
+		bp.start();
+		try {
+			primes.take();
+		} finally {
+			bp.cancel();
+		}
+	}
+}
